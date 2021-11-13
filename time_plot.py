562c1ee7293aec726a2e2b80437ea7a526a8abd9
lli259 <37081135+lli259@users.noreply.github.com>
2021-11-12 20:17:46 -0500

    Update time_plot.py

--- time_plot.py
+++ time_plot.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#1 pandas plot 
 time_ratio=pd.read_csv('a.csv')
 time_ratio=time_ratio.set_index(time_ratio.columns.values[0])
 time_ratio.index=range(len(time_ratio))
 
+#draw vertical and horizental line
 plt.axvline(x=30, color='b', linestyle='-')
 plt.axvline(x=53, color='b', linestyle='-')
 plt.axhline(y=1, color='r', linestyle='-')
@@ -12,7 +14,9 @@ time_ratio.plot()
 plt.legend()
 
 #plt.show()
+#save figure
 plt.savefig('time_ratio.png')
 
+#clear figure
 plt.clf()
 
